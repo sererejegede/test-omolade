74df5880187b9df8bbc14b7cb8a625af10a2ebd9
sererejegede <sererejegede@gmail.com>
2022-04-02 19:45:10 +0100

    chore: Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const formElement = document.getElementById("my_love");
+  const tableBody = document.querySelector('table tbody');
+
+  if (!formElement) return
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(formElement));
+    const tr = `<tr><td>${data.name}</td><td>${data.date}</td><td>${data.color}</td></tr>`;
+
+    tableBody.insertAdjacentHTML('beforeend', tr);
+
+    formElement.reset();
+  });
+});
